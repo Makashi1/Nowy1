638770c0e8ece73b60aa0dcd756ac0da8d7bbb6e
Makashi1 <kkanclerz79@gmail.com>
2019-11-12 20:09:08 +0100

    nowe zmiany 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 System.out .print("hello word");
     System.out.print("Publikijemy pierwszy program")
+    System.out.print("kolejna zmiana")
     }
 }
